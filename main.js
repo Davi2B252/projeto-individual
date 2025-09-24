bdc1c5f4dfd86c4843bb2dc9fe16578a935e95f9
Davi2B252 <davi.souza.soares@escola.pr.gov.br>
2025-09-23 21:41:08 -0300

    versão 1.0

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
+// Sorteia o número aleatório
+var randomnumber = Math.floor(Math.random() * 100) + 1;
+console.log("Número sorteado (debug): " + randomnumber);
 
+var guesses = 0;
+
+// função do palpite
+function checkGuess(userGuess) {
+    guesses++;
+
+    if (userGuess === randomnumber) {
+        console.log("Número correto!");
+        console.log("Tentativas: " + guesses);
+    } else if (userGuess > randomnumber) {
+        console.log("Tente um número menor!");
+    } else {
+        console.log("Tente um número maior!");
+    }
+if (guesses < 10) {
+    console.log("Você tem mais " + (10 - guesses) + " tentativas");
+} if (guesses >= 10) {
+    console.log("Você perdeu"); 
+}}
+
+displayImage("imagem-do-jogo.png");
+
+function displayImage(imagepatch) {
+	var img = document.createElement("img");
+	img.src = imagepath;
+	document.doby.appendChild(img);
+}
+
+//versão 1.0
